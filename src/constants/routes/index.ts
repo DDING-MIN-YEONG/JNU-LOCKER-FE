645bc8d3da6d3fbb301cca003c48a623d63f6e33
minbo <smb5174@naver.com>
2025-03-30 22:40:40 +0900

    feat: ROUTE 상수에 경로 추가

--- src/constants/routes/index.ts
+++ src/constants/routes/index.ts
@@ -4,4 +4,7 @@ export const ROUTE = {
   REGISTER_INFO: "/sign-up/register-info",
   DEPARTMENT_INFO: "/department-info",
   ENTER_EMAIL: "/enter-email",
+  DEPARTMENT_INFO_ANNOUNCEMENT: "/department-info/announcement",
+  DEPARTMENT_INFO_ANNOUNCEMENT_LIST: "/department-info/announcement/list",
+  APPLY_LOCKER: "/apply-locker",
 };
