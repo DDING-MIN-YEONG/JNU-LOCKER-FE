bda6f2c7c9386a0b5516af32d840346e6f10f21b
minbo <smb5174@naver.com>
2025-04-01 14:53:16 +0900

    feat: Txt 컴포넌트 구현

---
+++ src/components/design-system/Txt/index.tsx
@@ -0,0 +1,25 @@
+import classNames from "classnames/bind";
+import styles from "@/components/design-system/Txt/index.module.scss";
+import { ReactNode } from "react";
+
+const cn = classNames.bind(styles);
+
+interface TxtProps {
+  children: ReactNode;
+  className?: string;
+  size: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "small" | "tiny";
+  color?: "primary" | "error" | "white" | "black";
+  weight?: "normal" | "medium" | "semiBold" | "bold";
+  fontType?: "basic" | "chnam";
+}
+
+export default function Txt({
+  children,
+  className,
+  color = "black",
+  fontType = "basic",
+  size,
+  weight = "normal",
+}: TxtProps) {
+  return <span className={cn(size, color, fontType, weight, className)}>{children}</span>;
+}
