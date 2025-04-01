d78b9551742aad4452d15f74bed10b21577691dd
minbo <smb5174@naver.com>
2025-04-01 18:17:02 +0900

    refactor: Txt 컴포넌트의 size props 옵셔널로 변경

--- src/components/design-system/Txt/index.tsx
+++ src/components/design-system/Txt/index.tsx
@@ -7,7 +7,7 @@ const cn = classNames.bind(styles);
 interface TxtProps {
   children: ReactNode;
   className?: string;
-  size: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "small" | "tiny";
+  size?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "small" | "tiny";
   color?: "primary" | "error" | "white" | "black";
   weight?: "normal" | "medium" | "semiBold" | "bold";
   fontType?: "basic" | "chnam";
