c4b9570fad619adb0631718eb3aea6ba8e165859
minbo <smb5174@naver.com>
2025-04-01 22:26:59 +0900

    feat: secondary color 추가

--- src/components/design-system/Txt/index.tsx
+++ src/components/design-system/Txt/index.tsx
@@ -8,7 +8,7 @@ interface TxtProps {
   children: ReactNode;
   className?: string;
   size?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "small" | "tiny";
-  color?: "primary" | "error" | "white" | "black";
+  color?: "primary" | "secondary" | "error" | "white" | "black";
   weight?: "normal" | "medium" | "semiBold" | "bold";
   fontType?: "basic" | "chnam";
 }
