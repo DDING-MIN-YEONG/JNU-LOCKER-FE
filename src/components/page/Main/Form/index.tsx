99fe9ee248b2c95f510e3248a19b86339c764750
minbo <smb5174@naver.com>
2025-04-01 18:21:18 +0900

    refactor: 메인 페이지에 Txt 컴포넌트 적용

--- src/components/page/Main/Form/index.tsx
+++ src/components/page/Main/Form/index.tsx
@@ -5,6 +5,7 @@ import Button from "@/components/design-system/Button";
 import classNames from "classnames/bind";
 import styles from "@/components/page/Main/Form/index.module.scss";
 import useSignIn from "@/hooks/sign-in/useSignIn";
+import Txt from "@/components/design-system/Txt";
 
 const cn = classNames.bind(styles);
 
@@ -32,12 +33,14 @@ export default function SignInForm() {
         />
       </div>
       {error.isError && (
-        <p role="alert" className={cn("errorMessage")}>
+        <Txt size="tiny" color="error">
           {error.errorMessage}
-        </p>
+        </Txt>
       )}
       <Button type="submit" className={cn("signInBtn")}>
-        로그인
+        <Txt size="h4" weight="medium" color="white">
+          로그인
+        </Txt>
       </Button>
     </form>
   );
--- src/components/page/Main/index.tsx
+++ src/components/page/Main/index.tsx
@@ -3,6 +3,7 @@ import styles from "@/components/page/Main/index.module.scss";
 import SignInForm from "@/components/page/Main/Form/index";
 import Link from "next/link";
 import { ROUTE } from "@/constants/routes";
+import Txt from "@/components/design-system/Txt";
 
 const cn = classNames.bind(styles);
 
@@ -10,13 +11,25 @@ export default function Main() {
   return (
     <div className={cn("container")}>
       <div className={cn("titleContainer")}>
-        <p className={cn("title")}>전남대학교</p>
-        <p className={cn("subTitle")}>사물함 신청 서비스</p>
+        <Txt size="h1" fontType="chnam">
+          전남대학교
+        </Txt>
+        <Txt weight="medium" className={cn("subTitle")}>
+          사물함 신청 서비스
+        </Txt>
       </div>
       <SignInForm />
       <div className={cn("linkContainer")}>
-        <Link href={ROUTE.SIGN_UP}>회원가입</Link>
-        <Link href={ROUTE.ENTER_EMAIL}>비밀번호 재설정</Link>
+        <Link href={ROUTE.SIGN_UP}>
+          <Txt size="small" className={cn("link")}>
+            회원가입
+          </Txt>
+        </Link>
+        <Link href={ROUTE.ENTER_EMAIL}>
+          <Txt size="small" className={cn("link")}>
+            비밀번호 재설정
+          </Txt>
+        </Link>
       </div>
     </div>
   );
