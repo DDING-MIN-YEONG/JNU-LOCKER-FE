c04c293e926bc687e5b358a6ad62656eaa32ff79
minbo <smb5174@naver.com>
2025-04-01 18:03:04 +0900

    refactor: 기본 정보 등록 페이지에 Txt 컴포넌트 적용

--- src/components/page/RegisterInfo/Form/index.tsx
+++ src/components/page/RegisterInfo/Form/index.tsx
@@ -6,6 +6,7 @@ import classNames from "classnames/bind";
 import styles from "@/components/page/RegisterInfo/Form/index.module.scss";
 
 import useRegisterInfo from "@/hooks/sign-up/useRegisterInfo";
+import Txt from "@/components/design-system/Txt";
 
 const cn = classNames.bind(styles);
 
@@ -49,16 +50,20 @@ export default function RegisterInfoForm() {
         />
       </div>
       {error.isError && (
-        <p role="alert" className={cn("errorMessage")}>
+        <Txt size="tiny" color="error" className={cn("errorMessage")}>
           {error.errorMessage}
-        </p>
+        </Txt>
       )}
       <div className={cn("btnContainer")}>
-        <Button color="white" onClick={onPrevBtnClick} type="button" className={cn("nextBtn")}>
-          이전
+        <Button color="white" onClick={onPrevBtnClick} type="button" className={cn("prevBtn")}>
+          <Txt size="small" weight="medium">
+            이전
+          </Txt>
         </Button>
         <Button type="submit" className={cn("nextBtn")}>
-          완료
+          <Txt size="small" color="white" weight="medium">
+            완료
+          </Txt>
         </Button>
       </div>
     </form>
--- src/components/page/RegisterInfo/index.tsx
+++ src/components/page/RegisterInfo/index.tsx
@@ -3,12 +3,19 @@ import RegisterInfoForm from "@/components/page/RegisterInfo/Form/index";
 import ChnamLogo from "@/components/common/ChnamLogo";
 import classNames from "classnames/bind";
 import styles from "@/components/page/RegisterInfo/index.module.scss";
+import Txt from "@/components/design-system/Txt";
 
 const cn = classNames.bind(styles);
 
 export default function RegisterInfo() {
   return (
-    <SignUpLayout title="기본정보 등록하기">
+    <SignUpLayout
+      title={
+        <Txt size="h1" weight="semiBold">
+          기본정보 등록하기
+        </Txt>
+      }
+    >
       <div className={cn("imgContainer")}>
         <ChnamLogo width={103} height={98} />
       </div>
