040cabffe68f0ab7028fb4180eca356e92374bed
minbo <smb5174@naver.com>
2025-04-01 14:53:50 +0900

    refactor: ApplyHeader 컴포넌트에 Txt 컴포넌트 적용

--- src/components/common/ApplyHeader/index.tsx
+++ src/components/common/ApplyHeader/index.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames/bind";
 import styles from "@/components/common/ApplyHeader/index.module.scss";
 import Link from "next/link";
 import { ROUTE } from "@/constants/routes";
+import Txt from "@/components/design-system/Txt";
 
 const cn = classNames.bind(styles);
 
@@ -9,7 +10,9 @@ export default function ApplyHeader() {
   return (
     <header className={cn("header")}>
       <Link href={ROUTE.MAIN} className={cn("logo")}>
-        JNU-Locker
+        <Txt size="h2" fontType="chnam" color="white">
+          JNU-Locker
+        </Txt>
       </Link>
     </header>
   );
