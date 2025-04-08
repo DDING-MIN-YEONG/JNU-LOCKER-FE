b44b90b9862fe27ee10595b885855a5d2ebed111
minbo <smb5174@naver.com>
2025-04-08 23:57:04 +0900

    refactor: 위원회 회원가입 메인 페이지 수정

--- src/components/page/committee/SignUp/Form/index.tsx
+++ src/components/page/committee/SignUp/Form/index.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import TextInput from "@/components/common/TextInput";
 import Button from "@/components/design-system/Button";
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
 import Txt from "@/components/design-system/Txt";
 import useSignUp from "@/hooks/committee/sign-up/useSignUp";
+import { Selector } from "@/components/common/Selector";
+import { COMMITTEE_SIGN_UP_CATEGORY } from "@/constants/committee/sign-up";
 
 const cn = classNames.bind(styles);
 
 export default function SignUpForm() {
-  const { formAction, formData, onChange, error } = useSignUp();
+  const { formAction, formData, onChange, error, organizations, departments } = useSignUp();
 
   return (
     <form onSubmit={formAction} className={cn("form")}>
@@ -18,81 +19,26 @@ export default function SignUpForm() {
         회원가입
       </Txt>
       <div className={cn("inputContainer")}>
-        <TextInput
-          containerClassName={cn("emailInputContainer")}
-          id="email"
-          type="email"
-          label="전남대학교 이메일"
-          placeholder="이메일을 입력해주세요."
-          value={formData.email}
+        <Selector
+          label="유형"
           onChange={onChange}
-        >
-          <Button type="button" className={cn("emailBtn")}>
-            <Txt size="tiny" weight="bold" color="white">
-              메일전송
-            </Txt>
-          </Button>
-        </TextInput>
-        <TextInput
-          containerClassName={cn("certificationContainer")}
-          id="emailCertificationNumber"
-          type="text"
-          label="이메일 인증코드"
-          placeholder="이메일 인증코드를 입력해주세요."
-          value={formData.emailCertificationNumber}
-          onChange={onChange}
-        >
-          <Button type="button" className={cn("certificationBtn")}>
-            <Txt size="tiny" weight="bold" color="white">
-              인증하기
-            </Txt>
-          </Button>
-        </TextInput>
-        <TextInput
-          containerClassName={cn("phoneNumberInputContainer")}
-          id="phoneNumber"
-          type="text"
-          label="연락처"
-          placeholder="연락처를 입력해주세요."
-          value={formData.phoneNumber}
-          onChange={onChange}
-        >
-          <Button type="button" className={cn("certificationBtn")}>
-            <Txt size="tiny" weight="bold" color="white">
-              인증하기
-            </Txt>
-          </Button>
-        </TextInput>
-        <TextInput
-          containerClassName={cn("certificationContainer")}
-          id="phoneNumberCertificationNumber"
-          type="text"
-          label="연락처 인증코드"
-          placeholder="연락처 인증코드를 입력해주세요."
-          value={formData.phoneNumberCertificationNumber}
-          onChange={onChange}
-        >
-          <Button type="button" className={cn("certificationBtn")}>
-            <Txt size="tiny" weight="bold" color="white">
-              메일전송
-            </Txt>
-          </Button>
-        </TextInput>
-        <TextInput
-          id="password"
-          type="password"
-          label="비밀번호"
-          placeholder="비밀번호를 입력해주세요."
-          value={formData.password}
-          onChange={onChange}
+          options={COMMITTEE_SIGN_UP_CATEGORY}
+          value={formData.category.value}
+          id="category"
         />
-        <TextInput
-          id="passwordConfirm"
-          type="password"
-          label="비밀번호 확인"
-          placeholder="비밀번호를 다시 입력해주세요."
-          value={formData.passwordConfirm}
+        <Selector
+          label="소속"
           onChange={onChange}
+          options={organizations}
+          value={formData.affiliation.value}
+          id="affiliation"
+        />
+        <Selector
+          label="학과"
+          onChange={onChange}
+          options={departments}
+          value={formData.department.value}
+          id="department"
         />
       </div>
       {error.isError && (
