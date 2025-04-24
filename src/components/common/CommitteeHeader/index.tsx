f504b810a6dbce92a5443479d426c753f7238b5e
minbo <smb5174@naver.com>
2025-04-24 15:54:16 +0900

    refactor: 위원회용 헤더 컴포넌트 수정

--- src/components/common/CommitteeHeader/index.tsx
+++ src/components/common/CommitteeHeader/index.tsx
@@ -10,27 +10,32 @@ const cn = classNames.bind(styles);
 export default function CommitteeHeader() {
   return (
     <header className={cn("header")}>
-      <Link href={ROUTE.COMMITTEE.MAIN} className={cn("logo")}>
-        <ChnamLogo height={58} width={61} />
+      <Link href={ROUTE.COMMITTEE.APPLY_LIST} className={cn("logo")}>
+        <ChnamLogo height={50} width={55} />
         <Txt fontType="chnam" className={cn("headerTitle")}>
           전남대학교 사물함 신청 서비스
         </Txt>
       </Link>
-      <Link href={ROUTE.COMMITTEE.APPLY_LIST}>
-        <Txt className={cn("headerTitle")} weight="medium">
-          신청 목록
-        </Txt>
-      </Link>
-      <Link href={ROUTE.COMMITTEE.ANNOUNCEMENT}>
-        <Txt className={cn("headerTitle")} weight="medium">
-          공지사항
-        </Txt>
-      </Link>
-      <Link href={ROUTE.COMMITTEE.EVENT}>
-        <Txt className={cn("headerTitle")} weight="medium">
-          이벤트
-        </Txt>
-      </Link>
+      <div className={cn("linkContainer")}>
+        <Link href={ROUTE.COMMITTEE.APPLY_LIST}>
+          <Txt className={cn("headerTitle")} weight="medium">
+            신청 목록
+          </Txt>
+        </Link>
+        <Link href={ROUTE.COMMITTEE.ANNOUNCEMENT}>
+          <Txt className={cn("headerTitle")} weight="medium">
+            공지사항
+          </Txt>
+        </Link>
+        <Link href={ROUTE.COMMITTEE.EVENT}>
+          <Txt className={cn("headerTitle")} weight="medium">
+            이벤트
+          </Txt>
+        </Link>
+      </div>
+      <div className={cn("departmentContainer")}>
+        <Txt className={cn("headerTitle")}>수학과</Txt>
+      </div>
     </header>
   );
 }
