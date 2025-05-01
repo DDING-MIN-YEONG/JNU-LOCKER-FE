714390ffa0175e7a012e0de68e3fd2b6ebbb1e3b
minbo <smb5174@naver.com>
2025-05-01 23:43:14 +0900

    refactor: 이벤트 생성 폼 타입 변경, 이벤트 생성 요청 타입 추가

--- src/types/committee/event/index.ts
+++ src/types/committee/event/index.ts
@@ -2,17 +2,42 @@ export interface CreateEventForm {
   title: string;
   startAt: Date | null;
   endAt: Date | null;
-  participationDepartmentIds: number[];
+  affiliation: {
+    id: number;
+    value: string;
+  };
+  participationDepartmentIds: {
+    id: number;
+    value: string;
+  }[];
   floors: {
     floorNumber: null | number;
-    prefixes: [
-      {
-        lockerPrefix: string;
-        ranges: {
-          lockerStartNumber: null | number;
-          lockerEndNumber: null | number;
-        }[];
-      },
-    ];
+    floorId: number;
+    prefixes: {
+      prefixId: number;
+      lockerPrefix: string;
+      ranges: {
+        rangeId: number;
+        lockerStartNumber: null | number;
+        lockerEndNumber: null | number;
+      }[];
+    }[];
+  }[];
+}
+
+export interface CreateEventRequest {
+  title: string;
+  startAt: Date;
+  endAt: Date;
+  participationDepartmentIds: number[];
+  floors: {
+    floorNumber: number;
+    prefixes: {
+      lockerPrefix: string;
+      ranges: {
+        lockerStartNumber: number;
+        lockerEndNumber: number;
+      }[];
+    }[];
   }[];
 }
