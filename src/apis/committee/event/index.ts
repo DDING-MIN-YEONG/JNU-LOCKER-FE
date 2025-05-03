eb6ebe49bdcefd08a48e93d9708b3115c6c38dbb
minbo <smb5174@naver.com>
2025-05-03 23:43:04 +0900

    feat: 이벤트 목록 불러오는 api 연동

--- src/apis/committee/event/index.ts
+++ src/apis/committee/event/index.ts
@@ -1,6 +1,13 @@
+import { EventList } from "@/apis/dtos/committee/event";
 import { https } from "@/apis/https";
 import { CreateEventRequest } from "@/types/committee/event";
 
 export const postCreateEvent = async (formData: CreateEventRequest) => {
   await https.post("events", formData);
+};
+
+export const getEventList = async (page: number, size: number, direction: "asc" | "desc" = "asc") => {
+  const { data } = await https.get(`events?page=${page}&size=${size}&direction=${direction}`);
+
+  return new EventList(data);
 };
