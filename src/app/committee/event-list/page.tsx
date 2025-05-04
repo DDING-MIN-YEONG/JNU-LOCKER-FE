968fe9d0b55972530110b237c404576b8e95da95
minbo <smb5174@naver.com>
2025-05-05 00:39:20 +0900

    fix: useSearchParams로 인한 에러 수정

--- src/app/committee/event-list/page.tsx
+++ src/app/committee/event-list/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import CommitteeHeader from "@/components/common/CommitteeHeader";
 import EventList from "@/components/page/committee/event-list";
+import { Suspense } from "react";
 
 export default function EventListPage() {
   return (
     <>
       <CommitteeHeader />
-      <EventList />
+      <Suspense>
+        <EventList />
+      </Suspense>
     </>
   );
 }
