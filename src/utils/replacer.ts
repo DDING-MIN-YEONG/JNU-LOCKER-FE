66ca2adeed70ddf428a1a5eb2cb8018091ee1f68
minbo <smb5174@naver.com>
2025-05-05 21:26:13 +0900

    feat: 숫자로 바꿔주는 함수 구현

--- src/utils/replacer.ts
+++ src/utils/replacer.ts
@@ -3,3 +3,7 @@ export const phoneNumberReplace = (phoneNumber: string) =>
     .replace(/[^0-9]/g, "")
     .replace(/([0-9]{3})([0-9]{4})([0-9]{4})/, "$1-$2-$3")
     .slice(0, 13);
+
+export const numberReplace = (value: string) => {
+  return value.replace(/[^0-9]/g, "");
+};
