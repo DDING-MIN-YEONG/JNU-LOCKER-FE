f874cfb74951f41b77c92d1c32538c43a11f8301
minbo <smb5174@naver.com>
2025-05-05 12:27:02 +0900

    chore: validation 메시지 변경

--- src/constants/error/index.ts
+++ src/constants/error/index.ts
@@ -65,7 +65,7 @@ export const RESET_PASSWORD = {
 
 export const APPLY_LOCKER = {
   lockerNumber: {
-    [VALIDATION_TYPES.REQUIRED]: "신청할 사물함을 입력해 주세요.",
+    [VALIDATION_TYPES.REQUIRED]: "신청할 사물함 번호를 입력해 주세요.",
   },
 };
 
