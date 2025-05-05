190bfa264e360d97315c25c37ee3f1d8a921eb74
minbo <smb5174@naver.com>
2025-05-05 14:51:46 +0900

    feat: 사물함 정보를 불러오는 api 구현

---
+++ src/apis/student/apply-locker/index.ts
@@ -0,0 +1,13 @@
+import { Locker, LockerList } from "@/apis/dtos/student/locker";
+import { https } from "@/apis/https";
+
+export const getLockerList = async (eventId: number) => {
+  const { data } = await https.get(`events/${eventId}/lockers`);
+
+  const lockerList: LockerList[] = data.map(
+    ({ floorId, floorNumber, lockers }: { floorId: number; floorNumber: number; lockers: Locker[] }) =>
+      new LockerList({ floorId, floorNumber, lockers }),
+  );
+
+  return lockerList;
+};
