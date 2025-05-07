1d4691cdfaff937818ad3689dbc659214b4e3dd3
minbo <smb5174@naver.com>
2025-05-07 14:27:08 +0900

    feat: 특정 이벤트의 신청 목록 페이지에서 사용하는 페이지네이션 정보를 포함하는 훅 구현

---
+++ src/hooks/committee/event/useApplyDetailPagination.ts
@@ -0,0 +1,28 @@
+import { useRouter, useSearchParams } from "next/navigation";
+
+export const useApplyDetailPagination = (page: number, eventId: number) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const pagesPerGroup = 5;
+
+  const setPage = (page: number) => {
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set("page", page.toString());
+
+    router.replace(`?${newParams.toString()}`);
+  };
+
+  const queryParams: { page: number; size: number; direction: "asc" | "desc"; eventId: number } = {
+    page: page - 1,
+    size: 10,
+    direction: "asc",
+    eventId,
+  };
+
+  return {
+    setPage,
+    pagesPerGroup,
+    queryParams,
+  };
+};
