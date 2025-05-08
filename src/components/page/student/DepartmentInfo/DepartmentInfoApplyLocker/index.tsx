59ba2c56213cf456409527acd5a5101892570c3b
minbo <smb5174@naver.com>
2025-05-09 00:27:52 +0900

    feat: 학과 정보 페이지에 캐러셀 적용

--- src/components/page/student/DepartmentInfo/DepartmentInfoApplyLocker/index.tsx
+++ src/components/page/student/DepartmentInfo/DepartmentInfoApplyLocker/index.tsx
@@ -1,37 +1,26 @@
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
-import Link from "next/link";
-import { ROUTE } from "@/constants/routes";
 import Txt from "@/components/design-system/Txt";
+import MyLockerEventCarousel from "../MyLockerEventCarousel";
+import { EmblaOptionsType } from "embla-carousel";
+import { useGetMyEvent } from "@/hooks/tanstack-query/student/department-info/useGetMyEvent";
 
 const cn = classNames.bind(styles);
 
 export default function DepartmentInfoApplyLocker() {
+  const { data, isError, isPending } = useGetMyEvent();
+  const OPTIONS: EmblaOptionsType = { loop: true };
+
+  if (isError || isPending) {
+    return;
+  }
+
   return (
     <div className={cn("container")}>
       <Txt color="secondary" weight="bold" size="h3">
         사물함 신청
       </Txt>
-      <div className={cn("applyContainer")}>
-        <Txt className={cn("applyTitle")} weight="bold" size="h3">{`2025-1 사물함 신청`}</Txt>
-        <div className={cn("applyContentContainer")}>
-          <Txt size="small">신청 시작 시간 : 2025.03.09 (일) 10:00</Txt>
-          <Txt size="small">신청 종료 시간 : 2025.03.10 (월) 18:00</Txt>
-          <Txt size="small">사물함 여석 개수 : 20개</Txt>
-        </div>
-        <Link href={ROUTE.STUDENT.APPLY_LOCKER} className={cn("applyLink")}>
-          <Txt color="white" weight="medium" size="h6">
-            신청하러 가기
-          </Txt>
-        </Link>
-      </div>
-      <div className={cn("moreLinkContainer")}>
-        <Link href={ROUTE.STUDENT.DEPARTMENT_INFO_ANNOUNCEMENT_LIST}>
-          <Txt className={cn("moreLink")} size="tiny">
-            더보기
-          </Txt>
-        </Link>
-      </div>
+      <MyLockerEventCarousel myEventList={data} options={OPTIONS} />
     </div>
   );
 }
