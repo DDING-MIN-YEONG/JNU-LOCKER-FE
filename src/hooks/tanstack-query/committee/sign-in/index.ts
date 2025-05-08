2ab608e8a759b0da8176a4b829febc12f5ab9636
minbo <smb5174@naver.com>
2025-05-09 00:37:47 +0900

    feat: 로그인 시 나의 정보 api 쿼리 무효화

--- src/hooks/tanstack-query/committee/sign-in/index.ts
+++ src/hooks/tanstack-query/committee/sign-in/index.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { ROUTE } from "@/constants/routes";
 import { AxiosError } from "axios";
@@ -8,6 +8,7 @@ import { postSignIn } from "@/apis/common/sign-in";
 export const useCommitteeSignIn = () => {
   const router = useRouter();
   const { mutate } = useCommitteeSignInMutate();
+  const queryClient = useQueryClient();
 
   const onCommitteeSignIn = (formData: SignInFormData) => {
     mutate(formData, {
@@ -15,6 +16,7 @@ export const useCommitteeSignIn = () => {
         alert(error.response?.data.message || "로그인에 실패했습니다.");
       },
       onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["myInfo"] });
         router.push(ROUTE.COMMITTEE.APPLY_LIST);
         alert("로그인에 성공했습니다.");
       },
--- src/hooks/tanstack-query/student/sign-in/index.ts
+++ src/hooks/tanstack-query/student/sign-in/index.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { ROUTE } from "@/constants/routes";
 import { AxiosError } from "axios";
@@ -8,6 +8,7 @@ import { postSignIn } from "@/apis/common/sign-in";
 export const useStudentSignIn = () => {
   const router = useRouter();
   const { mutate } = useStudentSignInMutate();
+  const queryClient = useQueryClient();
 
   const onStudentSignIn = (formData: SignInFormData) => {
     mutate(formData, {
@@ -15,6 +16,7 @@ export const useStudentSignIn = () => {
         alert(error.response?.data.message || "로그인에 실패했습니다.");
       },
       onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["myInfo"] });
         router.push(ROUTE.STUDENT.DEPARTMENT_INFO);
         alert("로그인에 성공했습니다.");
       },
