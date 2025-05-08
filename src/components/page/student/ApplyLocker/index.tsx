79b0b33c61eaee7e4028375979af373e15aefca2
minbo <smb5174@naver.com>
2025-05-09 00:31:56 +0900

    feat: 사물함 신청 페이지에 나의 정보 불러오는 api 연동

--- src/components/page/student/ApplyLocker/index.tsx
+++ src/components/page/student/ApplyLocker/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import DepartmentLayout from "@/components/Layout/DepartmentLayout";
 
 import classNames from "classnames/bind";
@@ -5,12 +7,18 @@ import styles from "./index.module.scss";
 import LockerApplicationStatus from "@/components/page/student/ApplyLocker/LockerApplicationStatus/index";
 import ApplyLockerForm from "@/components/page/student/ApplyLocker/Form/index";
 import MyLockerApplicationStatus from "@/components/page/student/ApplyLocker/MyLockerApplicationStatus/index";
+import { useGetMyInfo } from "@/hooks/tanstack-query/common/my-info/useGetMyInfo";
 
 const cn = classNames.bind(styles);
 
 export default function ApplyLocker() {
+  const { data, isPending, isError } = useGetMyInfo();
+
+  if (isPending || isError) {
+    return;
+  }
   return (
-    <DepartmentLayout affiliation="공과대학" department="컴퓨터정보통신공학과" containerClassName={cn("layout")}>
+    <DepartmentLayout affiliation={data?.affiliation} containerClassName={cn("layout")}>
       <div className={cn("container")}>
         <LockerApplicationStatus />
         <div className={cn("formApplicationStatusContainer")}>
