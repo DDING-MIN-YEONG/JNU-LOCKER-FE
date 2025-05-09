a9640bf54241cc8c0cd993c8dd1b921d86e19c66
minbo <smb5174@naver.com>
2025-05-09 19:19:35 +0900

    feat: 학과 정보 페이지에서 내 학과를 보여주는 부분에 대한 로딩 처리

--- src/components/page/student/DepartmentInfo/index.tsx
+++ src/components/page/student/DepartmentInfo/index.tsx
@@ -6,13 +6,18 @@ import DepartmentInfoApplyLocker from "@/components/page/student/DepartmentInfo/
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
 import { useGetMyInfo } from "@/hooks/tanstack-query/common/my-info/useGetMyInfo";
+import Skeleton from "@/components/common/Skeleton";
 
 const cn = classNames.bind(styles);
 
 export default function DepartmentInfo() {
   const { data, isPending, isError } = useGetMyInfo();
 
-  if (isPending || isError) {
+  if (isPending) {
+    return <Skeleton className={cn("skeleton")} />;
+  }
+
+  if (isError) {
     return;
   }
 
