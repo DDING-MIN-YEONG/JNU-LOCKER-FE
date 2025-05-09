4ee34101d83c0e661c5a8615d97181c66cc9ca93
minbo <smb5174@naver.com>
2025-05-09 21:35:36 +0900

    feat: 나의 정보를 불러오는 api의 상태 코드가 401일 때 로그인 페이지로 이동

--- src/components/Layout/AffiliationContainer/index.tsx
+++ src/components/Layout/AffiliationContainer/index.tsx
@@ -5,18 +5,29 @@ import styles from "./index.module.scss";
 import AffiliationLabel from "@/components/common/AffiliationLabel";
 import { useGetMyInfo } from "@/hooks/tanstack-query/common/my-info/useGetMyInfo";
 import Skeleton from "@/components/common/Skeleton";
+import { useRouter } from "next/navigation";
+import { ROUTE } from "@/constants/routes";
+import { useEffect } from "react";
 
 const cn = classNames.bind(styles);
 
 export default function AffiliationContainer() {
-  const { data, isPending, isError } = useGetMyInfo();
+  const router = useRouter();
+  const { data, isPending, isError, error } = useGetMyInfo();
+
+  useEffect(() => {
+    if (isError && error.response.status === 401) {
+      alert("로그인 후 이용해주세요.");
+      router.push(ROUTE.STUDENT.MAIN);
+    }
+  }, [isError, error, router]);
 
   if (isPending) {
     return <Skeleton className={cn("skeleton")} />;
   }
 
   if (isError) {
-    return;
+    return null;
   }
 
   return (
