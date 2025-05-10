5689cae459bf4c79b1659a7a88fc284a0f32c012
minbo <smb5174@naver.com>
2025-05-10 12:19:09 +0900

    feat: 이벤트 생성 시 이벤트 리스트 다시 불러오기

--- src/hooks/tanstack-query/committee/event/useCreateEvent.ts
+++ src/hooks/tanstack-query/committee/event/useCreateEvent.ts
@@ -5,11 +5,16 @@ import { postCreateEvent } from "@/apis/committee/event";
 import { CreateEventForm, CreateEventRequest } from "@/types/committee/event";
 import { convertToKST } from "@/functions/date";
 import { ApiResponseError } from "@/types/common/api";
+import { useQueryKeys } from "../../common/useQueryKeys";
 
 export const useCreateEvent = () => {
   const router = useRouter();
   const { mutate } = useCreateEventMutate();
   const queryClient = useQueryClient();
+
+  const eventQueryKeys = useQueryKeys(["eventList"]);
+
+  const applyQueryKeys = useQueryKeys(["applyList"]);
 
   const onCreateEvent = (formData: CreateEventForm) => {
     if (!formData.title) {
@@ -65,8 +70,14 @@ export const useCreateEvent = () => {
         alert(error.response.data.message || "이벤트 생성에 실패하였습니다.");
       },
       onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["eventList", 0, 4, "desc"] });
-        queryClient.invalidateQueries({ queryKey: ["applyList", 0, 4, "desc"] });
+        eventQueryKeys.forEach((queryKey) => {
+          queryClient.invalidateQueries({ queryKey });
+        });
+
+        applyQueryKeys.forEach((queryKey) => {
+          queryClient.invalidateQueries({ queryKey });
+        });
+
         router.push(ROUTE.COMMITTEE.EVENT_LIST);
         alert("이벤트 생성에 성공하셨습니다.");
       },
