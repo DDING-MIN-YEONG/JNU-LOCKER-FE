c9cd5c017607495ce0579b1f8c62448c329648e7
minbo <smb5174@naver.com>
2025-05-14 16:14:24 +0900

    feat: 공지사항 get, put 관련 tanstack query 커스텀 훅 구현

---
+++ src/hooks/tanstack-query/committee/announcement/usePutAnnouncement.ts
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ApiResponseError } from "@/types/common/api";
+import { PutAnnouncementFormRequest } from "@/types/committee/announcement";
+import { putAnnouncement } from "@/apis/committee/announcement";
+
+export const usePutAnnouncement = (announcementId: string) => {
+  const { mutate } = usePutAnnouncementMutate();
+  const queryClient = useQueryClient();
+
+  const onPutAnnouncement = (formData: PutAnnouncementFormRequest) => {
+    mutate(
+      { formData, announcementId },
+      {
+        onError: (error) => {
+          alert(error.response.data.message || "공지사항 수정에 실패하였습니다.");
+        },
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["announcement", announcementId] });
+
+          alert("공지사항 수정에 성공하셨습니다.");
+        },
+      },
+    );
+  };
+  return {
+    onPutAnnouncement,
+  };
+};
+
+export const usePutAnnouncementMutate = () => {
+  return useMutation<void, ApiResponseError, { formData: PutAnnouncementFormRequest; announcementId: string }>({
+    mutationKey: ["putAnnouncement"],
+    mutationFn: putAnnouncement,
+  });
+};
