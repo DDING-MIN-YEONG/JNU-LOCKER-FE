d39feb83f0a547bfacb42b4d3263cbbbd8e91ab1
minbo <smb5174@naver.com>
2025-05-15 23:58:01 +0900

    feat: 나의 학과에 해당하는 공지사항 상세 페이지 구현

---
+++ src/app/student/my-announcement/[announcementId]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import MyAnnouncementDetail from "@/components/page/student/my-announcement";
+import { Suspense } from "react";
+
+export default function MyAnnouncementDetailPage() {
+  return (
+    <>
+      <Suspense>
+        <MyAnnouncementDetail />
+      </Suspense>
+    </>
+  );
+}
---
+++ src/components/page/student/my-announcement/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import classNames from "classnames/bind";
+import styles from "./index.module.scss";
+import Txt from "@/components/design-system/Txt";
+import MyAnnouncementDetailInfoForm from "@/components/page/student/my-announcement/MyAnnouncementDetailInfoForm";
+import { useMyAnnouncementDetailForm } from "@/hooks/student/announcement/useMyAnnouncementDetailForm";
+import Skeleton from "@/components/common/Skeleton";
+
+const cn = classNames.bind(styles);
+
+export default function MyAnnouncementDetail() {
+  const { formData, isPending, isError } = useMyAnnouncementDetailForm();
+
+  if (isPending) {
+    return <Skeleton className={cn("skeleton")} />;
+  }
+
+  if (isError) {
+    return null;
+  }
+
+  return (
+    <div className={cn("container")}>
+      <Txt color="primary" size="h3" weight="medium">
+        공지사항
+      </Txt>
+      <MyAnnouncementDetailInfoForm formData={formData} />
+    </div>
+  );
+}
