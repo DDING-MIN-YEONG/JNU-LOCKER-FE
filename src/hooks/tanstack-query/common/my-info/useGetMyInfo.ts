a726825d05353123854c64f5254bbd111ca4b636
minbo <smb5174@naver.com>
2025-05-16 16:33:29 +0900

    feat: useGetMyInfo 훅에 에러 타입 추가

--- src/hooks/tanstack-query/common/my-info/useGetMyInfo.ts
+++ src/hooks/tanstack-query/common/my-info/useGetMyInfo.ts
@@ -1,9 +1,10 @@
 import { getMyInfo } from "@/apis/common/my-info";
 import { MyInfo } from "@/apis/dtos/common/my-info";
+import { ApiResponseError } from "@/types/common/api";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetMyInfo = () => {
-  return useQuery<MyInfo>({
+  return useQuery<MyInfo, ApiResponseError>({
     queryKey: ["myInfo"],
     queryFn: () => getMyInfo(),
   });
