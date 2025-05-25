3891d51f2af519ce27c1436435a9a45a7346dfa7
minbo <smb5174@naver.com>
2025-05-25 17:44:18 +0900

    refactor: createEventValidator의 리턴값을 alert 메시지로 변경

--- src/functions/validator/createEventValidator.ts
+++ src/functions/validator/createEventValidator.ts
@@ -3,30 +3,25 @@ import { CreateEventForm } from "@/types/committee/event";
 
 export const createEventValidator = (formData: CreateEventForm) => {
   if (!formData.title) {
-    alert(CREATE_EVENT_VALIDATION.title.required);
-    return false;
+    return CREATE_EVENT_VALIDATION.title.required;
   }
 
   if (!formData.startAt) {
-    alert(CREATE_EVENT_VALIDATION.startAt.required);
-    return false;
+    return CREATE_EVENT_VALIDATION.startAt.required;
   }
 
   if (!formData.endAt) {
-    alert(CREATE_EVENT_VALIDATION.endAt.required);
-    return false;
+    return CREATE_EVENT_VALIDATION.endAt.required;
   }
 
   if (formData.participationDepartmentIds.length === 0) {
-    alert(CREATE_EVENT_VALIDATION.department.required);
-    return false;
+    return CREATE_EVENT_VALIDATION.department.required;
   }
 
   const invalidFloorNumber = formData.floors.find((floor) => floor.floorNumber === null || floor.floorNumber < 1);
 
   if (invalidFloorNumber) {
-    alert(CREATE_EVENT_VALIDATION.floor.min);
-    return false;
+    return CREATE_EVENT_VALIDATION.floor.min;
   }
 
   const invalidLockerNumber = formData.floors.find((floor) =>
@@ -36,9 +31,8 @@ export const createEventValidator = (formData: CreateEventForm) => {
   );
 
   if (invalidLockerNumber) {
-    alert(CREATE_EVENT_VALIDATION.lockerNumber.required);
-    return false;
+    return CREATE_EVENT_VALIDATION.lockerNumber.required;
   }
 
-  return true;
+  return null;
 };
