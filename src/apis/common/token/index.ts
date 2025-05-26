7e4fa69e58d7be7de510fbda9af8ddbfe717edaa
minbo <smb5174@naver.com>
2025-05-26 16:59:57 +0900

    refactor: reissue 함수 로직 변경

--- src/apis/common/token/index.ts
+++ src/apis/common/token/index.ts
@@ -1,10 +1,22 @@
 import { LoginInfo } from "@/apis/dtos/common/my-info";
-import { baseInstance } from "@/apis/instance/baseInstance";
+import axios from "axios";
 
 export const postReissue = async () => {
   try {
-    const { data } = await baseInstance.post("auth/reissue", {});
+    const refreshToken = localStorage.getItem("refreshToken");
 
+    const { data } = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}v1/auth/reissue`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+        },
+      },
+    );
+
+    localStorage.setItem("accessToken", data.accessToken);
+    localStorage.setItem("refreshToken", data.refreshToken);
     return new LoginInfo(data);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
