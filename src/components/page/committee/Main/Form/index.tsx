0d48080aecd7224d13fc16616b6de995a70a2f5f
minbo <smb5174@naver.com>
2025-05-31 22:16:48 +0900

    feat: 위원회 로그인시 로딩 처리

--- src/components/page/committee/Main/Form/index.tsx
+++ src/components/page/committee/Main/Form/index.tsx
@@ -8,52 +8,56 @@ import TextInput from "@/components/common/TextInput";
 import useCommitteeSignInForm from "@/hooks/committee/sign-in/useCommitteeSignInForm";
 import Link from "next/link";
 import { ROUTE } from "@/constants/routes";
+import Spinner from "@/components/common/Spinner";
 
 const cn = classNames.bind(styles);
 
 export default function CommitteeSignInForm() {
-  const { formAction, formData, error, onInputChange } = useCommitteeSignInForm();
+  const { formAction, formData, error, onInputChange, isLoginLoading } = useCommitteeSignInForm();
 
   return (
-    <form onSubmit={formAction} className={cn("form")}>
-      <Txt weight="semiBold" className={cn("title")}>
-        로그인
-      </Txt>
-      <div className={cn("inputContainer")}>
-        <TextInput
-          id="email"
-          type="email"
-          label="이메일"
-          placeholder="이메일을 입력해주세요."
-          value={formData.email}
-          onChange={onInputChange}
-        />
-        <TextInput
-          id="password"
-          type="password"
-          label="비밀번호"
-          placeholder="비밀번호를 입력해주세요."
-          value={formData.password}
-          onChange={onInputChange}
-        />
-      </div>
-      {error.isError && (
-        <Txt size="tiny" color="error">
-          {error.errorMessage}
-        </Txt>
-      )}
-      <Button type="submit" className={cn("completeBtn")}>
-        <Txt size="h5" color="white" weight="medium">
+    <>
+      <form onSubmit={formAction} className={cn("form")}>
+        <Txt weight="semiBold" className={cn("title")}>
           로그인
         </Txt>
-      </Button>
-      <div className={cn("linkContainer")}>
-        <Link href={ROUTE.COMMITTEE.SIGN_UP}>
-          <Txt size="small" className={cn("link")}>
-            회원가입
+        <div className={cn("inputContainer")}>
+          <TextInput
+            id="email"
+            type="email"
+            label="이메일"
+            placeholder="이메일을 입력해주세요."
+            value={formData.email}
+            onChange={onInputChange}
+          />
+          <TextInput
+            id="password"
+            type="password"
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요."
+            value={formData.password}
+            onChange={onInputChange}
+          />
+        </div>
+        {error.isError && (
+          <Txt size="tiny" color="error">
+            {error.errorMessage}
           </Txt>
-        </Link>
-      </div>
-    </form>
+        )}
+        <Button type="submit" className={cn("completeBtn")} disabled={isLoginLoading}>
+          <Txt size="h5" color="white" weight="medium">
+            로그인
+          </Txt>
+        </Button>
+        <div className={cn("linkContainer")}>
+          <Link href={ROUTE.COMMITTEE.SIGN_UP}>
+            <Txt size="small" className={cn("link")}>
+              회원가입
+            </Txt>
+          </Link>
+        </div>
+      </form>
+      {isLoginLoading && <Spinner />}
+    </>
   );
 }
