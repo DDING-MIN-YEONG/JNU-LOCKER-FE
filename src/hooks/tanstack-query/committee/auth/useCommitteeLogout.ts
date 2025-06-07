eae75769f0ccd07f53d66db078630caaa6cf39fb
minbo <smb5174@naver.com>
2025-06-07 22:54:00 +0900

    feat: 위원회용 페이지에 로그아웃 기능 추가

---
+++ src/hooks/tanstack-query/committee/auth/useCommitteeLogout.ts
@@ -0,0 +1,37 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { ROUTE } from "@/constants/routes";
+import { ApiResponseError } from "@/types/common/api";
+import { postLogout } from "@/apis/common/my-info";
+
+export const useCommitteeLogout = () => {
+  const router = useRouter();
+  const { mutate, isPending } = useCommitteeLogoutMutate();
+  const queryClient = useQueryClient();
+
+  const onCommitteeLogout = () => {
+    mutate(undefined, {
+      onError: (error) => {
+        alert(error.response.data.message || "로그아웃에 실패했습니다.");
+      },
+      onSuccess: () => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        router.push(ROUTE.COMMITTEE.MAIN);
+        alert("로그아웃에 성공했습니다.");
+        queryClient.clear();
+      },
+    });
+  };
+  return {
+    onCommitteeLogout,
+    isLogoutLoading: isPending,
+  };
+};
+
+export const useCommitteeLogoutMutate = () => {
+  return useMutation<void, ApiResponseError>({
+    mutationKey: ["committeeLogout"],
+    mutationFn: postLogout,
+  });
+};
