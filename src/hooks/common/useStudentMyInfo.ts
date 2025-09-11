617b033a6ff18aad9222e48cced5896e833bb3ed
smb0123 <smb5174@naver.com>
2025-09-11 22:29:12 +0900

    refactor: interceptors로직 변경
    - api의 상태코드가 401이고 access token을 갱신하는 로직도 401이면 기존 api의 error를 return

--- src/hooks/common/useStudentMyInfo.ts
+++ src/hooks/common/useStudentMyInfo.ts
@@ -2,15 +2,13 @@ import { useRouter } from "next/navigation";
 import { useGetMyInfo } from "@/hooks/tanstack-query/common/my-info/useGetMyInfo";
 import { useEffect } from "react";
 import { ROUTE } from "@/constants/routes";
-import { REFRESH_TOKEN_ERROR_MESSAGE } from "@/constants/axios";
-import { ApiResponseErrorWithMessage } from "@/types/common/api";
 
 export const useStudentMyInfo = () => {
   const router = useRouter();
   const { data, isPending, isError, error } = useGetMyInfo();
 
   useEffect(() => {
-    if (isError && (error as ApiResponseErrorWithMessage)?.message === REFRESH_TOKEN_ERROR_MESSAGE) {
+    if (isError && error.response.status === 401) {
       alert("로그인 후 이용해주세요.");
       router.push(ROUTE.STUDENT.MAIN);
     }
--- src/hooks/tanstack-query/common/my-info/useGetMyInfo.ts
+++ src/hooks/tanstack-query/common/my-info/useGetMyInfo.ts
@@ -1,10 +1,10 @@
 import { getMyInfo } from "@/apis/common/my-info";
 import { MyInfo } from "@/apis/dtos/common/my-info";
-import { ApiResponseErrorWithMessage, ApiResponseError } from "@/types/common/api";
+import { ApiResponseError } from "@/types/common/api";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetMyInfo = () => {
-  return useQuery<MyInfo, ApiResponseErrorWithMessage | ApiResponseError>({
+  return useQuery<MyInfo, ApiResponseError>({
     queryKey: ["myInfo"],
     queryFn: () => getMyInfo(),
   });
