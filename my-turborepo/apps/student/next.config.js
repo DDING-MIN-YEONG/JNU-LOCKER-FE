1c33532905091feddec7463caba6302d7a9db903
smb0123 <smb5174@naver.com>
2025-09-14 13:28:37 +0900

    chore: student 폴더에 대한 초기 세팅

--- my-turborepo/apps/student/next.config.js
+++ my-turborepo/apps/student/next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+  sassOptions: {
+    additionalData: `@import "src/styles/globals.scss";`,
+  },
+});
