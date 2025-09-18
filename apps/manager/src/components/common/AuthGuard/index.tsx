cde31c9050eba3d1af2555119bd11f0494aaa64a
smb0123 <smb5174@naver.com>
2025-09-18 22:46:14 +0900

    refactor: AuthGuard의 라우팅에 대해 alert 추가

--- apps/manager/src/components/common/AuthGuard/index.tsx
+++ apps/manager/src/components/common/AuthGuard/index.tsx
@@ -28,6 +28,7 @@ export default function AuthGuard({ children }: AuthGuardProps) {
 
     // CORS 에러 처리
     if (isCorsError(error) && !isServerInspectionPage) {
+      alert("서버 점검중입니다.");
       router.push(ROUTE.SERVER_INSPECTION);
       return;
     }
@@ -38,6 +39,7 @@ export default function AuthGuard({ children }: AuthGuardProps) {
 
       // 인증이 필요한 페이지에서 401 에러가 발생한 경우 로그인 페이지로 리다이렉트
       if (statusCode === 401 && (!isUnauthorizedPage || isServerInspectionPage)) {
+        alert("로그인 후 이용해주세요.");
         router.push(ROUTE.MAIN);
       }
     }
