92d0fff97883cf7d0ff2ab26252080888002ef13
smb0123 <smb5174@naver.com>
2025-09-23 22:20:27 +0900

    refactor: TextInput 컴포넌트 제거

--- apps/manager/src/components/page/Main/Form/index.tsx
+++ apps/manager/src/components/page/Main/Form/index.tsx
@@ -2,8 +2,8 @@
 
 import { ROUTE } from "@/constants/routes";
 import useCommitteeSignInForm from "@/hooks/sign-in/useCommitteeSignInForm";
+import { LabeledInput } from "@repo/ui/common/LabeledInput/index";
 import Spinner from "@repo/ui/common/Spinner/index";
-import TextInput from "@repo/ui/common/TextInput/index";
 import Button from "@repo/ui/design-system/Button/index";
 import Txt from "@repo/ui/design-system/Txt/index";
 import classNames from "classnames/bind";
@@ -22,7 +22,7 @@ export default function CommitteeSignInForm() {
           로그인
         </Txt>
         <div className={cn("inputContainer")}>
-          <TextInput
+          <LabeledInput
             id="email"
             type="email"
             label="이메일"
@@ -30,7 +30,7 @@ export default function CommitteeSignInForm() {
             value={formData.email}
             onChange={onInputChange}
           />
-          <TextInput
+          <LabeledInput
             id="password"
             type="password"
             label="비밀번호"
--- packages/ui/src/common/LabeledInput/index.tsx
+++ packages/ui/src/common/LabeledInput/index.tsx
@@ -1,16 +1,21 @@
 import classNames from "classnames/bind";
-import styles from "./index.module.scss";
 import { InputHTMLAttributes } from "react";
+import Label from "../../design-system/Label";
+import styles from "./index.module.scss";
 
 const cn = classNames.bind(styles);
 
 export interface LabeledInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "id"> {
   id: string;
-  type: "text" | "password" | "email";
   className?: string;
   labelClassName?: string;
   labeledInputContainerClassName?: string;
   label: string;
+  labelSize?: "small" | "medium" | "large";
+  labelColor?: "primary" | "secondary" | "error" | "white" | "black";
+  labelWeight?: "normal" | "medium" | "semiBold" | "bold";
+  labelFontType?: "basic" | "chnam";
+  required?: boolean;
 }
 
 export function LabeledInput({
@@ -19,13 +24,26 @@ export function LabeledInput({
   labelClassName,
   label,
   id,
+  labelSize = "medium",
+  labelColor = "black",
+  labelWeight = "medium",
+  labelFontType = "basic",
+  required = false,
   ...props
 }: LabeledInputProps) {
   return (
-    <div className={cn(labeledInputContainerClassName)}>
-      <label htmlFor={id} className={cn("label", labelClassName)}>
+    <div className={cn("container", labeledInputContainerClassName)}>
+      <Label
+        htmlFor={id}
+        className={labelClassName}
+        size={labelSize}
+        color={labelColor}
+        weight={labelWeight}
+        fontType={labelFontType}
+        required={required}
+      >
         {label}
-      </label>
+      </Label>
       <input {...props} id={id} className={cn("input", className)} />
     </div>
   );
